095f9df70207943dbf8c6d2c46bdc64fe86fb040
ozaki <fs65ugumori@yahoo.co.jp>
2019-11-18 08:15:01 +0900

    update Items component

--- pages/items/index.js
+++ pages/items/index.js
@@ -2,17 +2,8 @@ import { ListGroup, ListGroupItem, Panel } from 'react-bootstrap';
 import Layout from '../../components/Layout';
 import useQiitaApi from '../../hooks/useQiitaApi';
 
-function Item({ id, title, created_at, url, user: { name } }) {
-  return (
-    <ListGroupItem href={url}>
-      {title} <small>{created_at}</small>
-    </ListGroupItem>
-  );
-}
-
 function Items() {
   const { data, loading } = useQiitaApi();
-  console.log(data);
   return (
     <Layout>
       <Panel>
@@ -21,8 +12,10 @@ function Items() {
           <Panel.Body>loading...</Panel.Body>
         ) : (
           <ListGroup>
-            {data.map(item => (
-              <Item key={item.id} {...item} />
+            {data.map(({ id, title, created_at, url, user: { name } }) => (
+              <ListGroupItem key={id} href={url}>
+                {title} <small>{created_at}</small>
+              </ListGroupItem>
             ))}
           </ListGroup>
         )}
